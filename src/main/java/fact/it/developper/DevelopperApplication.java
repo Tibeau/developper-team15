045a946b1525fe235d005a5501bfb362bd7831ec
Tibeau <45169641+Tibeau@users.noreply.github.com>
2021-10-08 11:44:49 +0200

    first commit

---
+++ src/main/java/fact/it/developper/DevelopperApplication.java
@@ -0,0 +1,13 @@
+package fact.it.developper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopperApplication.class, args);
+    }
+
+}
