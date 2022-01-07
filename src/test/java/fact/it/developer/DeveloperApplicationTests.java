dc1f154a6fc9535c46137aea8a4abd2ec5364fd5
unknown <r0746415@student.thomasmore.be>
2022-01-07 16:12:02 +0100

    applicationtest

---
+++ src/test/java/fact/it/developer/DeveloperApplicationTests.java
@@ -0,0 +1,12 @@
+package fact.it.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class DeveloperApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
+
