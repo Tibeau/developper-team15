72c8c1842f578a7fd7de0f4b530faef560773889
unknown <r0746415@student.thomasmore.be>
2022-01-07 16:25:01 +0100

    change of name

--- src/test/java/fact/it/developer/DeveloperControllerIntegrationTests.java
+++ src/test/java/fact/it/developer/DeveloperControllerIntegrationTests.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 
-public class DeveleporControllerIntegrationTests {
+public class DeveloperControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/fact/it/developer/DeveloperControllerUnitTests.java
+++ src/test/java/fact/it/developer/DeveloperControllerUnitTests.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 
-public class DeveleporControllerUnitTests {
+public class DeveloperControllerUnitTests {
 
 
     @Autowired
